9ced1c98aea29f4b414c46c664314042b74409dc
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-07 09:05:39 +0800

    use tkinter

--- finding_highest_number.py
+++ finding_highest_number.py
@@ -1,6 +1,18 @@
 # Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
 
 # pseudocode
+# use tkinter
+import tkinter
+from tkinter import ttk
+
+window = tkinter.Tk()
+window.title("FINDING THE BIGGEST NUMBER")
+window.geometry('925x500+300+200')
+window.wm_resizable(False,False)
+
 # Ask for name
 # Input 3 numbers
 # finding the hisghest number
+
+
+window.mainloop()
