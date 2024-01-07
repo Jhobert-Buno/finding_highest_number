233f817add8225d30f7b355f8e89ed040fec3e7c
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-07 10:19:20 +0800

    insert background

--- finding_highest_number.py
+++ finding_highest_number.py
@@ -4,15 +4,21 @@
 # use tkinter
 import tkinter
 from tkinter import ttk
+from PIL import ImageTk, Image
+from tkinter import PhotoImage
 
 window = tkinter.Tk()
 window.title("FINDING THE BIGGEST NUMBER")
 window.geometry('925x500+300+200')
 window.wm_resizable(False,False)
 
+image_path=PhotoImage(file=r"C:\BsCpE 1st Year\Programming Logic and Design\Python Projects\Assignment #4\finding_highest_number\Background for python.png")
+bg_image=tkinter.Label(window,image=image_path)
+bg_image.place(relheight=1, relwidth=1)
+
 # Ask for name
 # Input 3 numbers
-# finding the hisghest number
+# finding the highest number
 
 
 window.mainloop()
